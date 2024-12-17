4abeb3f8b0563750e11069c6f04c8a9f4172dbb0
SnowHam1212 <hibi.3104.4649@gmail.com>
2024-12-17 18:34:53 +0900

    2024/12/17

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App'; // Appコンポーネントをインポート
 
-test('renders learn react link', () => {
+// テストケース
+test('renders the App component without crashing', () => {
+  // Appコンポーネントをレンダリングする
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // テキストが画面に表示されているか確認する
+  const linkElement = screen.getByText(/hello world/i); // "Hello World" にマッチするテキストを検索
+  expect(linkElement).toBeInTheDocument(); // 要素がDOMに存在することを確認
 });
